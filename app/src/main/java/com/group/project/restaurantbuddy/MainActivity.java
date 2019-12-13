d4494a216fd1ca04d65de4de977f3ab1dc3ba2d8
Bobby Mavrov <bobby.mavrov@kbwebconsult.com>
2019-12-12 18:17:54 -0800

    Fixing totals

--- app/src/main/java/com/group/project/restaurantbuddy/MainActivity.java
+++ app/src/main/java/com/group/project/restaurantbuddy/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
